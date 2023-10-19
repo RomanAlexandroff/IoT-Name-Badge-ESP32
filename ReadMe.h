f106a8743d0ee4b585450c866fa759e55cb237fd
RomanAlexandroff <r.aleksandroff@gmail.com>
2023-10-19 13:05:46 +0200

    ReadMe update

--- ReadMe.h
+++ ReadMe.h
@@ -35,8 +35,9 @@
       - WeAct 2.9'' E-paper black/white display;
       - LiPol Battery, *230mAh, 3.7V   
       * charge-measuring function in other.h uses capacity-specific constants, hence the 230mAh capacity
-        is important. To use batteries with different capacities, the constants need to be recalculated,
-        otherwise the readings will not be precise. See the instructions at the bottom of the page.
+        is important. To use batteries with different capacities, the constants may need to be
+        recalculated, otherwise the readings will not be precise. See the instructions at the bottom
+        of the page.
 
       
       The project is based on Jean-Marc Zingg's GxEPD2 library for E-paper displays:
@@ -74,32 +75,11 @@
 
       CALCULATING CONSTANTS FOR BATTERY CHARGE FUNCTION
 
-      You will need to install the following little utility onto your ESP:
+      You will need to check the following little utility:
       https://github.com/RomanAlexandroff/ESP-Battery-Charge-Utility
-      It has instructions inside of it that are similar to the text below.
       
-      First, you need to find out so called Battery_Max_Reading. Fully charge your battery,
-      connect it to your ESP, measure the battery state with "adc1_get_raw(ADC1_CHANNEL_0)" command
-      and output the measurements into Serial monitor. Battery_Max_Reading is the first number
-      outputed to Serial monitor. You will need it later.
-
-      Second, you need to find out so called Battery_Min_Reading. Let ESP connected to Serial 
-      monitor until the battery drowns down and ESP browns-out. Battery_Min_Reading is the last 
-      number outputed to Serial monitor.
-
-      The 1st constant equals to Battery_Min_Reading. You already have that value. In this sketch
-      it equals to 650 for a 230mAh battery. For other batteries the value may be different.
-
-      The 2nd constant is so called Battery_Coefficient. In this sketch Battery_Coefficient equals
-      to 12.62 for a 230mAh battery. For other batteries the number may be different.
-      Battery_Coefficient needs to be calculated by the following formula:
-
-      Battery_Coefficient == (Battery_Max_Reading - Battery_Min_Reading) / 100
-
-
-      Put the obtained constants into the sketch accordingly:
-
-      battery = ceil((adc1_get_raw(ADC1_CHANNEL_0) - Battery_Min_Reading) / Battery_Coefficient);
+      It was designed to be a universal solution for battery charge level detection in ESP-based
+      projects and has all the instructions inside of it.
     
 
   *********************************************************************************************** */
