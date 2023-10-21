558a85f0026bd8ad648eeaaa4503ac50129320e2
RomanAlexandroff <r.aleksandroff@gmail.com>
2023-10-21 13:02:33 +0200

    add failed OTA managing commands

--- ota_mode.h
+++ ota_mode.h
@@ -18,29 +18,56 @@
 /*                                                                                                */
 /* ********************************************************************************************** */
 
-short ft_ota_mode(String chat_id) 
+inline void ft_compose_message(String ssid, IPAddress ip, String chat_id)
+{
+    String  message;
+
+    message = "OTA mode activated. \n\nConnected to\n" + String(ssid);
+    message += "\n\nConnect to this Wi-Fi and go to the link\nhttp://";
+    message += String(ip[0]) + "." + String(ip[1]) + "." + String(ip[2]) + "." + String(ip[3]) + "/update";
+    message += "\n\nTo cancel the OTA update write any other command.";
+    bot.sendMessage(chat_id, message, "");
+}
+
+inline void ft_ElegantOTA_callbacks(String chat_id)
+{
+    ElegantOTA.onStart([chat_id]() {
+        DEBUG_PRINTF("\nOTA update process started.\n", "");
+        bot.sendMessage(chat_id, "Receiving new firmware...", "");
+    });
+    ElegantOTA.onEnd([chat_id](bool success) {
+        if (success)
+        {
+            DEBUG_PRINTF("OTA update completed successfully.", "");
+            bot.sendMessage(chat_id, "Success! OTA update is complete", "");
+        }
+        else
+        {
+            DEBUG_PRINTF("OTA update failed.", "");
+            bot.sendMessage(chat_id, "Oups! Something went wrong and OTA update was not completed", "");
+            delay(1000);
+            bot.sendMessage(chat_id, "Let's try again", "");
+        }
+    });
+}
+
+short  ft_ota_mode(String chat_id) 
 {
     String      ssid;
     IPAddress   ip;
-    String      message;
 
     ssid = WiFi.SSID();
     ip = WiFi.localIP();
     DEBUG_PRINTF("\n\nSOFT TRACKER\nOTA update mode initialized.\n\n", "");
     DEBUG_PRINTF("Wi-Fi network: %s\n", ssid.c_str());
     DEBUG_PRINTS("IP address: %d.%d.%d.%d\n\n", ip[0], ip[1], ip[2], ip[3]);
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { request->send(200, "text/plain", "Soft Tracker OTA update."); });
-    AsyncElegantOTA.begin(&server);
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { request->send(200, "text/plain", "Soft Tracker OTA update."); });        //Starting the OTA server
+    ElegantOTA.begin(&server);
+    ft_ElegantOTA_callbacks(chat_id);
     server.begin();
     DEBUG_PRINTF("HTTP server started.\nConnect to the stated Wi-Fi network and proceed to the link\n\n", "");
     DEBUG_PRINTS("http://%d.%d.%d.%d/update\n\n\n", ip[0], ip[1], ip[2], ip[3]);
-    message = "OTA mode activated. Version " + String(SOFTWARE_VERSION) + "\n\nConnected to\n" + String(ssid);
-    message += "\nAssigned IP\n" + String(ip[0]) + "." + String(ip[1]) + "." + String(ip[2]) + "." + String(ip[3]);
-    message += "\n\nConnect to the stated Wi-Fi network and go to the link\nhttp://";
-    message += String(ip[0]) + "." + String(ip[1]) + "." + String(ip[2]) + "." + String(ip[3]) + "/update";
-    message += "\n\nRemember that in OTA mode I will not go to sleep automatically.";
-    message += " To cancel the OTA mode without firmware update use \"off\" or \"reboot\" commands";
-    bot.sendMessage(chat_id, message, "");
+    ft_compose_message(ssid, ip, chat_id);
     return (-32767);
 }
  
