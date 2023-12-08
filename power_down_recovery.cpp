f09d361486a7d7426dad4398233baa9627106c60
RomanAlexandroff <r.aleksandroff@gmail.com>
2023-12-08 19:45:14 +0100

    refactor restart reason detection

--- power_down_recovery.cpp
+++ power_down_recovery.cpp
@@ -17,26 +17,19 @@
 
 void  ft_power_down_recovery(void)
 {
-    RTC_DATA_ATTR static long  power_loss_detector;
-    esp_reset_reason_t         reason;
+    esp_reset_reason_t  reason;
 
-    if (power_loss_detector != 1110111)
-    {
-        g_cycle_counter = 0;
-        g_power_on = false;
-        g_reboot = false;
-        g_panic = false;
-        power_loss_detector = 1110111;
-        DEBUG_PRINTF("Power-down Recovery was performed.\nThe cycle counter was set to %d\n", g_cycle_counter);
-    }
     reason = esp_reset_reason();
     switch (reason)
     {
         case ESP_RST_POWERON:
         case ESP_RST_BROWNOUT:
-            DEBUG_PRINTF("\nReset reason: Power-on or Brown-out reset\n", "");
+            g_cycle_counter = 0;
             g_power_on = true;
-            ft_delay(3000);
+            g_reboot = false;
+            g_panic = false;
+            DEBUG_PRINTF("\nReset reason: Power-on or Brown-out reset\n", "");
+            DEBUG_PRINTF("Power-down Recovery was performed.\nThe cycle counter was set to %d\n", g_cycle_counter);
             break;
         case ESP_RST_SW:
             DEBUG_PRINTF("\nReset reason: Software reset\n", "");
